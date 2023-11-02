15e6c362fe0ef66024a8200837a721498043362e
black-hole-fear <lucas19980227>
2023-11-02 20:17:17 +0100

    fastapi server socket

---
+++ server/FastAPI.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.websockets import WebSocket
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"You said: {data}")
